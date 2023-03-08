cbc731a8019c7872989551224cd3020102880cd9
hiron0413 <hikan_behase@icloud.com>
2023-03-08 20:02:30 +0900

    update contents / improve UI

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $(function() {
-    var d = new Date().getMonth() + 1
-    var icon_id = document.getElementById("icon")
+    var d = new Date().getMonth() + 1;
+    var icon_id = document.getElementById("icon");
+    const works_categories = ["project", "scratch", "web", "art"];
     if (3 <= d && d <= 5) {
         document.documentElement.style.setProperty("--season-color", "#a7e42e"); //テーマカラーを季節で変更
         icon_id.href = "./images/hiron0413-icon-spring.png" //アイコンを季節で変更
@@ -17,41 +18,37 @@ $(function() {
 
     var season_color = document.documentElement.style.getPropertyValue("--season-color");
     
-    $("#filter-item-all").css("background-color", season_color)
+    $("#filter-item-project").css("background-color", season_color);
+    select("project");
 
-    //ALL
-    $("#filter-item-all").click(function(){
-        var season_color = document.documentElement.style.getPropertyValue("--season-color");
-        var white = document.documentElement.style.getPropertyValue("--white");
-        
-        $("#filter-item-all").css("background-color", season_color)
-        $("#filter-item-scratch").css("background-color", white)
-        $("#filter-item-web").css("background-color", white)
-        $(".scratch").css("display", "block")
-        $(".web").css("display", "block")
-    });
+    //PROJECTS
+    $("#filter-item-project").click(() => select("project"));
 
     //SCRATCH
-    $("#filter-item-scratch").click(function(){
-        var season_color = document.documentElement.style.getPropertyValue("--season-color");
-        var white = document.documentElement.style.getPropertyValue("--white");
-        
-        $("#filter-item-scratch").css("background-color", season_color)
-        $("#filter-item-all").css("background-color", white)
-        $("#filter-item-web").css("background-color", white)
-        $(".web").css("display", "none")
-        $(".scratch").css("display", "block")
-    });
+    $("#filter-item-scratch").click(() => select("scratch"));
 
     //WEB
-    $("#filter-item-web").click(function(){
+    $("#filter-item-web").click(() => select("web"));
+
+    //ART
+    $("#filter-item-art").click(() => select("art"));
+
+    function select(item) {
         var season_color = document.documentElement.style.getPropertyValue("--season-color");
         var white = document.documentElement.style.getPropertyValue("--white");
         
-        $("#filter-item-web").css("background-color", season_color)
-        $("#filter-item-all").css("background-color", white)
-        $("#filter-item-scratch").css("background-color", white)
-        $(".scratch").css("display", "none")
-        $(".web").css("display", "block")
-    });
+        works_categories.forEach(c => {
+            if (item === "all") {
+                $("." + c).css("display", "block");
+            } else {
+                $("." + c).css("display", "none");
+            }
+            $("#filter-item-" + c).css("background-color", white);
+        });
+        if (item !== "all") {
+            $("." + item).css("display", "block");
+        }
+        $("#filter-item-all").css("background-color", white);
+        $("#filter-item-" + item).css("background-color", season_color);
+    }
 });
